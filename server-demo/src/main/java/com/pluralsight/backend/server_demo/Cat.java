6f02bd3e22974e7ea7f7cd530ee604ff13f81431
Jacob Lockhart <procoder165@gmail.com>
2024-12-09 14:55:28 -0500

    created server demo

---
+++ server-demo/src/main/java/com/pluralsight/backend/server_demo/Cat.java
@@ -0,0 +1,11 @@
+package com.pluralsight.backend.server_demo;
+
+public class Cat {
+    private String name;
+    private String catchPhrase;
+
+    public Cat(String name, String catchPhrase) {
+        this.name = name;
+        this.catchPhrase = catchPhrase;
+    }
+}
---
+++ server-demo/src/main/java/com/pluralsight/backend/server_demo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.pluralsight.backend.server_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/")
+    public Cat sayHello(){
+        return new Cat("Bai Bai", "Squeak");
+    }
+}
+
